c104dcf2473b6a1f6787fe86d22886e7ddf34b45
Leonardo <leonardo.bongianni@gmail.com>
2021-03-25 16:13:33 +0100

    Aggiunta funzione checked

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,27 +2,32 @@ var app = new Vue ({
   el: "#root",
     data: {
       inputTxt: "",
-      toDoList: [
-        {
-          title: "xx",
-        },
-        {
-          title: "yy",
-        },
-        {
-          title: "zz",
-        },
-      ],
+      toDoList: [],
+    },
+    computed: {
+      toDoListComputed: function() {
+        let toDoListDone = this.toDoList.filter((todoitem) => todoitem.status == "done");
+        let toDoListToDo = this.toDoList.filter((todoitem) => todoitem.status == "todo");
+
+        return {...toDoListToDo,...toDoListDone};
+      }
     },
     methods: {
       add: function(){
         if (this.inputTxt!="") {
           let obj = {
-            title: this.inputTxt
+            title: this.inputTxt,
+            status: "todo"
           }
           this.toDoList.push(obj);
-          this.inputTxt="";
+          this.toDoList.reverse();
+          this.inputTxt= "";
+          console.log(this.toDoList);
         }
+      },
+      checked: function(index){
+        console.log(this.toDoList[index].status);
+        this.toDoList[index].status = "done"
       }
     }
 
